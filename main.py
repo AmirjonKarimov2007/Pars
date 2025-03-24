0aad21c88a6c76386956e5b23b49d3f9ed454045
amirjon <amirjon@amirjons-MacBook-Air.local>
2025-03-24 13:52:53 +0500

    deploy

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import asyncio
-from playwright.async_api import async_playwright, Playwright
-import os
-path_to_extension = "/home/amirjon/Documents/web_Scraping/coursera_login/ezyZip"
+from playwright.async_api import async_playwright
+
+path_to_extension = "/Users/amirjon/Documents/parsing/Pars/capsolve"
 user_data_dir = "user_dataa"
 
-async def run(playwright: Playwright):
+async def run(playwright):
+    browser_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    
     context = await playwright.chromium.launch_persistent_context(
         user_data_dir,
         headless=False,
+        executable_path=browser_path,
         args=[
             f"--disable-extensions-except={path_to_extension}",
             f"--load-extension={path_to_extension}",
         ],
     )
 
-    # Create a new page in the context
-    page = await context.new_page()
+    page = context.pages[0] if context.pages else await context.new_page()
 
-    # Navigate to the desired URL
-    await page.goto("https://www.coursera.org/?authMode=signup")
+    # ✅ Sahifani yuklash tezroq bo‘lishi uchun 'domcontentloaded' ishlatamiz
+    await page.goto("https://www.coursera.org/?authMode=signup", wait_until="domcontentloaded")
+    # Login sahifasining automatlashishi
+    # Name uchun bo'lim
+    await page.wait_for_selector("input[name='name']", state="visible", timeout=5000)
+    await page.locator("input[name='name']").fill("Amirjon Karimov")
+    
+    # ✅ Email inputni yuklanishini kutish va to‘ldirish
+    await page.wait_for_selector("input[name='email']", state="visible", timeout=5000)
+    await page.locator("input[name='email']").fill("your_email@example.com")
+    # ✅Password uchun bo'lim
+    await page.wait_for_selector("input[name='password']", state="visible", timeout=5000)
+    await page.locator("input[name='password']").fill("Karimoff2007")
+    # ✅ "Join for Free" tugmasini bosish
+    await page.wait_for_selector("button[data-e2e='signup-form-submit-button']", state="visible", timeout=5000)
+    await page.locator("button[data-e2e='signup-form-submit-button']").click()
 
-    # You can add more code here to interact with the page or wait as needed
-    await page.wait_for_timeout(10000)  # Wait for 10 seconds before closing (optional)
-    import time
-    time.sleep(100)
+        # ⏳ Kutish vaqtini qisqartiramiz yoki olib tashlaymiz
+    await page.wait_for_timeout(10000000)  # 1 sekund kifoya
 
 async def main():
     async with async_playwright() as playwright:
